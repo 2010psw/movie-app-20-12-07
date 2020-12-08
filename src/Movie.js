639baf39e06b94505342f32aa944f06c9068dc2a
psw <2010psw@naver.com>
2020-12-08 23:27:26 +0900

    오류제거

--- src/Movie.js
+++ src/Movie.js
@@ -10,11 +10,9 @@ function Movie({ year, title, summary, poster, genres }) {
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">{genre}</li>
-          ))}
+          {genres && genres.map( (genre, index) => <li key={index} className="genres__genre">{genre}</li>)}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0,140)}...</p>
       </div>
     </div>
   );
@@ -26,7 +24,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
